8e279be5477e01e7a2c439c645a9760cc0592395
Markus Hällfors <markus@hallfors.com>
2025-05-07 09:36:05 +0300

    Add masters and candidate theses

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import sigchiDefault from "./assets/SIGCHI-talk.png";
 import sigchiHover from "./assets/sigchi.png";
 import myjyuDefault from "./assets/myjyu-default.png";
 import myjyuHover from "./assets/myjyu-hover.png";
+import mastersThesis from "./assets/masters-thesis.png";
+import candidateThesis from "./assets/candidate-thesis.png";
 
 const App = () => (
     <div className="container-fluid">
@@ -50,6 +52,28 @@ const App = () => (
                             <NavLink className="link-style" to="/SIGCHI-project">
                                 "How to advocate for Fair Design as a UX Designer" talk at SIGCHI Spring 2024 Seminar
                             </NavLink>
+                        </h4>
+                    </div>
+                    <div className="col project content-padding">
+                        <a className="link-style" href="https://jyx.jyu.fi/jyx/Record/jyx_123456789_87309">
+                            <ImageHover mainImage={mastersThesis} hoverImage={mastersThesis} />
+                        </a>
+                        <h4>
+                            <a className="link-style" href="https://jyx.jyu.fi/jyx/Record/jyx_123456789_87309">
+                                "Just let me buy my thing!" : A survey study on consumers' perceptions of social influence in E-Commerce 
+                            </a>
+                        </h4>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col project content-padding">
+                        <a className="link-style" href="https://jyx.jyu.fi/jyx/Record/jyx_123456789_72034">
+                            <ImageHover mainImage={candidateThesis} hoverImage={candidateThesis} />
+                        </a>
+                        <h4>
+                            <a className="link-style" href="https://jyx.jyu.fi/jyx/Record/jyx_123456789_72034">
+                                Dark patterns and their use in e-commerce
+                            </a>
                         </h4>
                     </div>
                     <div className="col project content-padding">
